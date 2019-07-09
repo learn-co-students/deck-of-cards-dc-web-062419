31a564c93a140cea3f6251f16a20707348590f64
Luke O'Malley <lukeomalley95@gmail.com>
2019-07-09 15:12:44 +0000

    Done.

--- solution.rb
+++ solution.rb
@@ -3,9 +3,13 @@ class Deck
   attr_reader :cards
   def initialize
     @cards = []
-    52.times do
-      @cards << Card.new(["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"].sample, ["Hearts", "Clubs", "Diamonds", "Spades"].sample)
+  
+    ["Hearts", "Clubs", "Diamonds", "Spades"].each do |suit|
+      ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"].each do |rank|
+        @cards << Card.new(suit, rank)
+      end
     end
+    
   end
   
   def choose_card
